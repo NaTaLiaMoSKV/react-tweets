c8f72a50544fb5227d089d904f599cb092b60221
Natalia Moskvychova <natashamoskv@gmail.com>
2023-04-26 03:02:44 +0300

    update project structure

--- src/components/TweetsExamples/TweetsExamples.js
+++ src/components/TweetsExamples/TweetsExamples.js
@@ -6,9 +6,9 @@ export default function TweetsExamples() {
             <h2 className={css.examples__title}>Always with you</h2>
             <div className={css.examples__items}>
                 <div className={css.examples__item}>
-                    <img className={css.examples__image} src={require('../../images/example2.jpg')}></img>
-                    <img className={css.examples__image} src={require('../../images/example1.jpg')}></img>
-                    <img className={css.examples__image} src={require('../../images/example3.jpg')}></img>
+                    <img className={css.examples__image} alt="man typing a message on the phone" src={require('../../images/example2.jpg')}></img>
+                    <img className={css.examples__image} alt="girl walking and chatting" src={require('../../images/example1.jpg')}></img>
+                    <img className={css.examples__image} alt="man received a notification on his phone" src={require('../../images/example3.jpg')}></img>
                 </div>
             </div>
         </div>
